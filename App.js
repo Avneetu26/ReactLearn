ddad9b93fcf4a64d896da48f7551e0989a59e561
AvneetKaur <avneetu26@gmail.com>
2025-02-06 15:50:15 -0500

    first checking - intro module

---
+++ App.js
@@ -0,0 +1,13 @@
+const heading = React.createElement("h1", {id: "heading"}, "Hello From React!");
+
+const parent = React.createElement("div", 
+    {id: "parent"},
+    React.createElement("div",
+        {id: "child"},
+        [React.createElement("h1", {}, "This is h1 tag"), React.createElement("h2", {}, "This is h2 tag")]
+    )
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+root.render(parent);
